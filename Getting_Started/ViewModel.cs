0d8904abbc4954797e19b7a526183c77111bb60f
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-04-29 13:53:36 +0530

    Sample included

---
+++ Getting_Started/ViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Syncfusion.UI.Xaml.Kanban;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_Started
+{
+    public class TaskDetails
+    {
+        public TaskDetails()
+        {
+
+            Tasks = new ObservableCollection<KanbanModel>();
+
+            Tasks.Add(new KanbanModel()
+            {
+
+                Title = "Universal App",
+
+                ID = "27654",
+
+                Description = "Incorporate feedback into functional specifications",
+
+                Category = "Open",
+
+                ColorKey = "Low",
+
+                Tags = new string[] { "Deployment" },
+
+                ImageURL = new Uri("/images/People_Circle1.png", UriKind.RelativeOrAbsolute)
+            });
+
+
+            Tasks.Add(new KanbanModel()
+            {
+
+                Title = "Universal App",
+
+                ID = "29477",
+
+                Description = "Design functional specifications",
+
+                Category = "In Progress",
+
+                ColorKey = "Normal",
+
+                Tags = new string[] { "Design" },
+
+                ImageURL = new Uri("/images/People_Circle2.png", UriKind.RelativeOrAbsolute)
+            });
+
+
+            Tasks.Add(new KanbanModel()
+            {
+                Title = "Universal App",
+
+                ID = "25678",
+
+                Description = "Review preliminary software specifications",
+
+                Category = "Done",
+
+                ColorKey = "Low",
+
+                Tags = new string[] { "Analysis" },
+
+                ImageURL = new Uri("/images/People_Circle3.png", UriKind.RelativeOrAbsolute)
+            });
+
+
+            Tasks.Add(new KanbanModel()
+            {
+                Title = "Universal App",
+
+                ID = "6593",
+
+                Description = "Draft preliminary software specifications",
+
+                Category = "Review",
+
+                ColorKey = "High",
+
+                Tags = new string[] { "Analysis" },
+
+                ImageURL = new Uri("/images/People_Circle4.png", UriKind.RelativeOrAbsolute)
+
+            });
+        }
+        public ObservableCollection<KanbanModel> Tasks { get; set; }
+    }
+}
